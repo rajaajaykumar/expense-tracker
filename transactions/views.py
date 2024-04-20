5685f3db701ae39eb482ab24d63eb3404f458726
rajaajaykumar <rajaajaykumar6@gmail.com>
2024-04-21 00:54:22 +0530

    Updated views to get total for income, expense and balance

--- transactions/views.py
+++ transactions/views.py
@@ -2,15 +2,17 @@ from django.shortcuts import render
 from .models import Category, Transaction
 from .forms import TransactionForm
 
-# Create your views here.
+
 def dashboard(request):
     """
     View to display the dashboard, that includes three recent transactions, account sumary and a form to add new transaction.
     """
     recent_transactions = Transaction.objects.order_by("-date")[:3]
-    total_income = 0
-    total_expense = 0
-    total_balance = 0
+
+    all_transactions = Transaction.objects.all()
+    total_income = sum(transaction.amount for transaction in all_transactions if transaction.category.type == "Income") if all_transactions else 0
+    total_expense = sum(transaction.amount for transaction in all_transactions if transaction.category.type == "Expense") if all_transactions else 0
+    total_balance = total_income - total_expense
 
     if request.method == "POST":
         form = TransactionForm(request.POST)
@@ -41,9 +43,9 @@ def transactions(request):
         form = TransactionForm()
     
     all_transactions = Transaction.objects.all().order_by("-date")
-    total_income = 0
-    total_expense = 0
-    total_balance = 0
+    total_income = sum(transaction.amount for transaction in all_transactions if transaction.category.type == "Income") if all_transactions else 0
+    total_expense = sum(transaction.amount for transaction in all_transactions if transaction.category.type == "Expense") if all_transactions else 0
+    total_balance = total_income - total_expense
 
     # Check if there are no transactions
     if not all_transactions:
