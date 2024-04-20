69d9366d4454d3011dfbad601fa67769940c4013
rajaajaykumar <rajaajaykumar6@gmail.com>
2024-04-21 01:35:02 +0530

    Created custom admin class for how it's presented in admin interface

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Category, Transaction
 
-# Register your models here.
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ["date", "amount", "get_transaction_type", "note"]
+    list_filter = ["date"]
+    search_fields = ["note"]
+    
+    def get_transaction_type(self, transaction):
+        return transaction.category.type if transaction.category else None
+
+    get_transaction_type.short_description = "Type"
+
+admin.site.register(Transaction, TransactionAdmin)
 admin.site.register(Category)
-admin.site.register(Transaction)
