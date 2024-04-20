ab490686920443561ee0c41c1d11d5f4d5407de1
rajaajaykumar <rajaajaykumar6@gmail.com>
2024-04-21 00:15:57 +0530

    Created two views

--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Category, Transaction
 
 # Create your views here.
+def dashboard(request):
+    """
+    View to display the dashboard
+    """
+    context = {}
+    return render(request, "transactions/dashboard.html", context)
+
+
+def transactions(request):
+    """
+    View to display all the historical transactions
+    """
+    context = {}
+    return render(request, "transactions/transactions.html", context)
