8c91461046fdd5c4ead1984fc6b4bac1a5a014e4
rajaajaykumar <rajaajaykumar6@gmail.com>
2024-04-20 23:52:40 +0530

    Defined Category and Transaction models

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    TRANSACTION_TYPE_CHOICES = [
+        ("Expense", "Expense"),
+        ("Income", "Income")
+    ]
+
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=10, choices=TRANSACTION_TYPE_CHOICES)
+
+    def __str__(self) -> str:
+        return f"{self.type} - {self.name}"
+
+
+class Transaction(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True, editable=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    note = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self) -> str:
+        formatted_date = self.date.strftime("%Y-%m-%d %H:%M:%S")
+        return f"{formatted_date} --- {self.category} --- ₹{self.amount}"
