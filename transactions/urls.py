59b3fc25d059260570a3e04915f4736da8a290a7
rajaajaykumar <rajaajaykumar6@gmail.com>
2024-04-23 00:49:25 +0530

    Updated URL configuration file

--- transactions/urls.py
+++ transactions/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("dashboard/", views.dashboard, name="dashboard"),
     path("transactions/", views.transactions, name="transactions"),
+    path("transaction/<int:pk>/update/", views.update_transaction, name="update-transaction"),
+    path("transaction/<int:pk>/delete/", views.delete_transaction, name="delete-transaction"),
 ]
