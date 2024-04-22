bcd6382d54b4b6e85719943d07bcfa75c88e34ba
rajaajaykumar <rajaajaykumar6@gmail.com>
2024-04-23 00:43:37 +0530

    Made the date field editable

--- transactions/models.py
+++ transactions/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Category(models.Model):
@@ -16,9 +17,15 @@ class Category(models.Model):
 
 class Transaction(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    date = models.DateTimeField(auto_now_add=True, editable=True)
+    date = models.DateTimeField(auto_now_add=False, editable=True, blank=False, null=False)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     note = models.CharField(max_length=100, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            # Set the date field to the current date and time only if the object is being created
+            self.date = timezone.now()
+        super().save(*args, **kwargs)
 
     def __str__(self) -> str:
         formatted_date = self.date.strftime("%Y-%m-%d %H:%M:%S")
