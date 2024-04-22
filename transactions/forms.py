58191ccd0f2875132838065b10a8a07cd6d98df8
rajaajaykumar <rajaajaykumar6@gmail.com>
2024-04-23 00:44:25 +0530

    Iincluded an input field for the date

--- transactions/forms.py
+++ transactions/forms.py
@@ -4,9 +4,10 @@ from .models import Transaction
 class TransactionForm(forms.ModelForm):
     class Meta:
         model = Transaction
-        fields = ["amount", "category", "note"]
+        fields = ["amount", "date", "category", "note"]
         widgets = {
             "category": forms.Select(attrs={"placeholder": "Select Category"}),
+            "date": forms.DateInput(attrs={"type": "date"}),
             "amount": forms.NumberInput(attrs={"placeholder": "Amount (₹)"}),
             "note": forms.TextInput(attrs={"placeholder": "Add note"}),
         }
